93a380b5aa3860930abf8a3f4e21adba563d8b95
monitor1-developer <76256724+monitor1-developer@users.noreply.github.com>
2021-02-20 10:06:49 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+noseX = 0;
+noseY = 0;
+
+function preload() {
+	clown_nose = loadImage('https://i.postimg.cc/FRdqchK2/clownnose.jpg');
+}
+
+function setup() {
+	canvas = createCanvas(300, 300);
+	canvas.center();
+	video = createCapture(VIDEO);
+	video.size(300, 300);
+	video.hide();
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+	console.log("PoseNet model initialized.")
+}
+
+function gotPoses(results) {
+	if (results.length > 0) {
+		console.log(results);
+		noseX = results[0].pose.nose.x - 5;
+		noseY = results[0].pose.nose.y - 5;
+		console.log("nose x = " + results[0].pose.nose.x + "nose y = " + results[0].pose.nose.y);
+	}
+}
+
+function draw() {
+	image(video, 0, 0, 300, 300);
+	image(clown_nose, noseX, noseY, 30, 30);
+}
+
+function takeSnapshot() {
+	save('myFilterImage.png');
+}
+
+
+
+
+
